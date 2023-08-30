194a9d20fb535f78c71f5849535d60933800c675
Bilal Masokano <bimasokano@gmail.com>
2023-08-30 15:53:41 -0400

    Test commit

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print("I can make Python work")
+# %% 
+
+print("I can make Python working")
 print("Matt smells weirdly nice")
+
+import numpy as np
+
+a = np.array((0,1,2,3,4))
+print(a)
+
+
+x = np.array((3,4,5,6))
+print(x)
